7d3434a695bf6d45cbe12cb5e9334185891e1343
Gene <dumbmahirukinnie@yandex.ru>
2024-02-25 02:32:21 +0300

    добавление вкладок

--- testograph.py
+++ testograph.py
@@ -2,14 +2,21 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(
-        ft.Row(controls=[
-            ft.Text('A'),
-            ft.Text('B'),
-            ft.Text('C'),
-            ft.ElevatedButton('D!!')
-        ])
+    page.add(ft.Text('Тестограф', theme_style=ft.TextThemeStyle.TITLE_LARGE))
+    tabs = ft.Tabs(
+        tabs=[
+            ft.Tab(
+                text='Тесты',
+                content=ft.Container(
+                    content=ft.Text('Save me')
+                )
+            ),
+            ft.Tab(
+                text='Видео'
+            )
+        ]
     )
+    page.add(tabs)
 
 
 
