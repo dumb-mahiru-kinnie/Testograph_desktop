a8b96d393d8494cd059197cda34380dc7786964f
Gene <dumbmahirukinnie@yandex.ru>
2024-02-25 02:51:10 +0300

    добавил базу данных

--- testograph.py
+++ testograph.py
@@ -2,13 +2,14 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    page.horizontal_alignment = 'CENTER'
     page.add(ft.Text('Тестограф', theme_style=ft.TextThemeStyle.TITLE_LARGE))
     tabs = ft.Tabs(
         tabs=[
             ft.Tab(
                 text='Тесты',
                 content=ft.Container(
-                    content=ft.Text('Save me')
+                    content=ft.Text('')
                 )
             ),
             ft.Tab(
